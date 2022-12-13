707947ada8a476a14eded1c09968e1b0759d8591
ARSLANHAN Emre <o22102081@info23-31.iut45.univ-orleans.fr>
2022-12-13 12:30:56 +0100

    Modif page compte

--- tuto/views.py
+++ tuto/views.py
@@ -33,10 +33,11 @@ def auteur():
         )
 
 @app.route("/compte")
+@login_required
 def compte():
     return render_template(
-        "compte.html"
-        )
+        "compte.html",
+        user=current_user)
 
 class AuthorForm(FlaskForm):
     id = HiddenField('id')
@@ -62,6 +63,10 @@ def livre_auteur(id):
     return render_template(
         "livre_auteur.html",livres = get_livre_auteur(id)
         )
+
+@app.route("/auteur/nb_livre/<id>")
+
+
     
 
 
