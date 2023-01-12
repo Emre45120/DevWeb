5e027ccf1b1e51daeca18f99e0d4b92424496ad5
ARSLANHAN Emre <o22102081@info25-18.iut45.univ-orleans.fr>
2023-01-12 16:41:35 +0100

    Tqt

---
+++ tes/models.py
@@ -0,0 +1,84 @@
+from .app import db
+from flask_login import UserMixin
+from .app import login_manager
+
+class User(db.Model,UserMixin): 
+    username = db.Column(db.String(80), primary_key=True)
+    password = db.Column(db.String(80))
+    admin = db.Column(db.Boolean)
+
+    def get_id(self):
+        return self.username
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+
+    def __repr__(self) -> str:
+        return "%s" % (self.name)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Float)
+    title = db.Column(db.String(100))
+    img = db.Column(db.String(200))
+    url = db.Column(db.String(200))
+    author_id = db.Column(db.Integer, db.ForeignKey("author.id"))
+    author = db.relationship("Author", backref=db.backref("books", lazy="dynamic"))
+
+class Favorite(db.Model):
+    user_id = db.Column(db.String(80), db.ForeignKey("user.username") , primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey("book.id"), primary_key=True)
+    user = db.relationship("User", backref=db.backref("favorites", lazy="dynamic"))
+    book = db.relationship("Book", backref=db.backref("favorites", lazy="dynamic"))
+
+def __repr__ (self ):
+    return "<Book (%d) %s>" % (self.id , self.title)
+
+def get_sample():
+    return Book.query.all()
+
+def get_auteur():
+    return Author.query.all()
+
+def get_livre(id):
+    return Book.query.get(id)
+
+def get_auteur2(id):
+    return Author.query.get(id)
+
+def get_info_auteur(id):
+    return Author.query.filter(Author.id == id).all()
+
+def get_livre_auteur(id):
+    return Book.query.filter(Book.author_id == id).all()
+
+def get_nb_livre_auteur(id):
+    id_a = int(id) + 1
+    return Book.query.filter_by(Book.author_id == id_a).all()
+
+def ajouter_favoris(idUser,idLivre):
+    fav = Favorite(user_id=idUser,book_id=idLivre)
+    db.session.add(fav)
+    db.session.commit()
+
+def supprimer_favoris(idUser,idLivre):
+    fav = Favorite.query.filter_by(user_id=idUser,book_id=idLivre).first()
+    db.session.delete(fav)
+    db.session.commit()
+
+def get_favoris(idUser):
+    return Favorite.query.filter_by(user_id=idUser).all()
+
+@login_manager.user_loader
+def load_user(username):
+    return User.query.get(username) 
+
+def get_favorites_books(user_id):
+    favorites = Favorite.query.filter_by(user_id=user_id).all()
+    books = []
+    for favoris in favorites:
+        books.append(get_livre(favoris.book_id))
+    return books
+
+
---
+++ tes/views.py
@@ -0,0 +1,221 @@
+from .app import app
+from flask import render_template, request , redirect , url_for
+from .models import get_sample , get_auteur , get_auteur2 , get_info_auteur , get_livre_auteur , get_favorites_books
+from flask_wtf import FlaskForm
+from wtforms import StringField, HiddenField , PasswordField
+from wtforms.validators import DataRequired
+from .models import *
+from hashlib import sha256
+from flask_login import login_user, current_user , logout_user , login_required
+from .app import app,db
+from flask import flash
+
+
+
+@app.route("/")
+def home():
+    return render_template(
+        "home.html",
+        title = "My Books",
+        books = get_sample(),
+    )
+
+@app.route("/detail/<id>")
+def detail(id):
+    book = get_livre(id)
+    var = False
+    if current_user.is_authenticated:
+        for fav in current_user.favorites:
+            if fav.book_id == book.id:
+                var = True
+    return render_template(
+        "detail.html", var = var,
+        book=book)
+
+@app.route("/auteur")
+def auteur():
+    return render_template(
+        "auteur.html",auteurs = get_auteur()
+        )
+
+@app.route("/compte")
+@login_required
+def compte():
+    return render_template(
+        "compte.html",
+        favorites_books = get_favorites_books(current_user.username),
+        user=current_user)
+
+class AuthorForm(FlaskForm):
+    id = HiddenField('id')
+    name = StringField('Name', validators=[DataRequired()])
+
+@app.route("/edit/author/<int:id>")
+@login_required
+def edit_author(id):
+    a = get_auteur2(id)
+    f = AuthorForm(id=a.id, name=a.name)
+    return render_template(
+        "edit_author.html",
+        author=a, form=f)
+
+@app.route("/auteur/info/<id>")
+def info_auteur(id):
+    return render_template(
+        "info_auteur.html",auteurs = get_info_auteur(id)
+        )
+
+@app.route("/auteur/livre/<id>")
+def livre_auteur(id):
+    livres= get_livre_auteur(id)
+    return render_template(
+        "livre_auteur.html",livres = livres
+        )
+
+
+@app.route("/save/author/", methods=['POST',])
+def save_author():
+    a = None
+    f = AuthorForm()
+    if f.validate_on_submit():
+        id = int(f.id.data)
+        a = get_auteur2(id)
+        a.name = f.name.data
+        db.session.commit()
+        return redirect(url_for('afficher_author', id=a.id))
+    a = get_auteur2(int[f.id.data])
+    return render_template(
+        "edit_author.html",
+        author=a, form=f)
+
+@app.route("/author/<int:id>")
+def afficher_author(id):
+    a = get_auteur2(id)
+    return render_template(
+        "info_auteur.html",
+        author=a)
+
+class LoginForm(FlaskForm):
+    username = StringField("Username")
+    password = PasswordField("Password")
+    next = HiddenField()
+
+    def get_authenticated_user(self):
+        user = User.query.get(self.username.data)
+        if user is None:
+            return None
+        m = sha256()
+        m.update(self.password.data.encode())
+        passwd = m.hexdigest()
+        if user.password == passwd:
+            return user
+        return None
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+    f = LoginForm()
+    if not f.is_submitted():
+        f.next.data = request.args.get('next')
+    elif f.validate_on_submit():
+        user = f.get_authenticated_user()
+        if user: 
+            login_user(user)
+            next = f.next.data or url_for('home') 
+            return redirect(next)
+    return render_template(
+        "login.html",
+        form=f)
+
+    
+
+@app.route("/logout/")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+class BookForm(FlaskForm):
+    id = HiddenField('id')
+    title = StringField('Title', validators=[DataRequired()])
+    author = StringField('Author', validators=[DataRequired()])
+
+@app.route("/add/favorite/<book_id>", methods=['GET', 'POST',])
+@login_required
+def add_favorite(book_id):
+    book = Book.query.filter_by(id=book_id).first_or_404()
+    favorite = Favorite(user_id=current_user.username, book_id=book.id) 
+    db.session.add(favorite)
+    db.session.commit()
+    return redirect(url_for('compte'))
+
+@app.route("/remove/favorite/<book_id>", methods=['GET', 'POST',])
+@login_required
+def remove_favorite(book_id):
+    book = Book.query.filter_by(id=book_id).first_or_404()
+    favorite = Favorite.query.filter_by(user_id=current_user.username, book_id=book.id).first_or_404()
+    db.session.delete(favorite)
+    db.session.commit()
+    return redirect(url_for('compte'))
+
+class SearchForm(FlaskForm):
+    search = StringField('search', validators=[DataRequired()])
+
+@app.route("/search/", methods=['POST'])
+def search():
+    f = SearchForm()
+    mot = f.search.data
+    print(mot)
+    print(type(mot))
+    books= Book.query.filter(Book.title.like('%'+mot+'%')).all()
+    print(books)
+    return render_template(
+        "resultat.html",
+        form=f, result=books)
+
+
+# je veux cree une methode qui permet de cree un compte et de se connecter avec ce compte
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username")
+    password = PasswordField("Password")
+    next = HiddenField()
+
+    def get_authenticated_user(self):
+        user = User.query.get(self.username.data)
+        if user is None:
+            return None
+        m = sha256()
+        m.update
+        passwd = m.hexdigest()
+        if user.password == passwd:
+            return user
+        return None
+
+
+@app.route("/cree" , methods=['GET', 'POST'])
+def cree():
+    f = RegisterForm()
+    if f.validate_on_submit():
+        user = User.query.filter_by(username=f.username.data).first()
+        print(user)
+        if user is None:
+            m = sha256()
+            m.update(f.password.data.encode())
+            passwd = m.hexdigest()
+            user = User(username=f.username.data, password=passwd)
+            db.session.add(user)
+            print("ok")
+            db.session.commit()
+            return redirect(url_for('login'))
+        else:
+            flash("Le nom d'utilisateur existe déjà.")
+            print("no")      
+    return render_template(
+        "cree.html",
+        form=f)
+
+
+
+        
+
+
+
